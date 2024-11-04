530b6240c45bc67f25230dd01566438cfad0cacc
Jee Hwan <jeehwan98@naver.com>
2024-11-05 01:48:57 +0900

    feat: login action

--- front/src/context/auth/user-auth-form.tsx
+++ front/src/context/auth/user-auth-form.tsx
@@ -1,20 +1,11 @@
 "use client"
 
+import LoginForm from "./login-form"
+
 export default function UserAuthForm() {
   return (
     <div className="grid gap-6">
-      <form>
-        <input
-          className="border border-gray-400"
-          type="text"
-          name="name"
-        />
-        <input
-          className="border border-gray-400"
-          type="password"
-          name="password"
-        />
-      </form>
+      <LoginForm />
     </div>
   )
 };
