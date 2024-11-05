8cbfda262f51bde7e5b1c4dd9d2963de449a07df
Jee Hwan <jeehwan98@naver.com>
2024-11-05 13:22:53 +0900

    chore: update utils

--- front/src/lib/utils.ts
+++ front/src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+const capitalizeFirstLetter = (word: string): string => {
+  return (word.charAt(0).toUpperCase() + word.slice(1));
+}
+
+export {
+  capitalizeFirstLetter,
+}
